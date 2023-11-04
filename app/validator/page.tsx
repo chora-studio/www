f45a2bcd3bb76f1c204ad106a21606b45305f6f9
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2023-11-03 17:03:37 -0700

    update content

--- app/validator/page.tsx
+++ app/validator/page.tsx
@@ -32,23 +32,33 @@ const ValidatorPage = () => {
           </p>
           <p>
             {
-              'Chora Validator is currently operating on proof-of-stake blockchains in the Cosmos ecosystem. With proof-of-stake networks, validators stake tokens (i.e. lock up tokens in the network) and then earn rewards for successfully verifying and producing blocks. Validators are also penalized for downtime or acting maliciously. When a validator is penalized, a portion of the staked tokens are slashed (i.e. destroyed and cannot be recovered).'
+              'Chora Validator is currently operating on proof-of-stake blockchains in the Cosmos ecosystem. On proof-of-stake networks, validators stake tokens (i.e. lock up their tokens in the network) and then earn rewards for successfully verifying and producing blocks. Validators are also penalized for downtime or acting maliciously. When a validator is penalized, a portion of the staked tokens are slashed (i.e. destroyed and cannot be recovered).'
             }
           </p>
           <p>
-            {'In the Cosmos ecosystem, token holders stake their tokens with validators to earn staking rewards alongside validators. When a validator is penalized for downtime or acting maliciously, token holders staking with the validator are also penalized, i.e. a portion of staked tokens from each account staking with the validator is also slashed. As a result, token holders are incentivized to stake tokens with validators they trust in order to reduce the risk of lost assets and to ensure the security and integrity of the network.'
+            {'In the Cosmos ecosystem, token holders stake tokens with validators to earn rewards alongside validators. When a validator is penalized for downtime or acting maliciously, the token holders are also penalized, i.e. a portion of the staked tokens from each account staking with the validator is slashed. Token holders are therefore incentivized to stake tokens with validators they trust, which in turn reduces the risk of lost assets and helps to ensure the security and integrity of the network.'
             }
           </p>
           <h2>{'what about governance?'}</h2>
           <p>
             {
-              'In addition to verifying and producing blocks, validators on proof-of-stake networks in the Cosmos ecosystem also act as representatives in network governance. When staking with a validator in the Cosmos ecosystem, token holders are also delegating their voting power (i.e. one token, one vote) to the validator. Token holders have the option to cancel out their delegated votes by voting independently. If a token holder does not intend to weigh in and vote independently on every governance proposal, they should choose validators that they feel best represents their interests.'
+              'In addition to verifying and producing blocks, validators on proof-of-stake networks in the Cosmos ecosystem also act as representatives in network governance. When staking with a validator in the Cosmos ecosystem, token holders are also delegating their voting power (i.e. one token, one vote) to the validator. Token holders have the option to cancel out their delegated votes by voting independently. If a token holder does not intend to vote on every governance proposal independently, they should stake with validators that they think will best represent their interests.'
             }
           </p>
           <h2>{'what about other services?'}</h2>
           <p>
             {
-              'Although not required, validators (or companies running validators) often provide additional tools and services as public goods for the benefit of the network. For example, some validators may run additional infrastructure such as full nodes, archive nodes, relayers, data oracles, or a validator on the corresponding test network, some validators may contribute to building or maintaining software, some validators may organize working groups and publish research, etc. Token holders may consider staking with validators that bring additional value to the network.'
+              'Although not required, validators (or companies running validators) often provide additional tools and services as public goods for the benefit of the network. For example, some may run additional infrastructure such as full nodes, archive nodes, relayers, oracle services, or a testnet validator on the corresponding test network, some may contribute to designing, building, or maintaining software, some may organize working groups and publish their research, etc. Token holders may want to consider staking with validators that bring additional and diverse forms of value to the network.'
+            }
+          </p>
+          <h2>{'why choose chora validator?'}</h2>
+          <p>
+            {'Chora Validator is committed to ensuring the security and integrity of every network that Chora Validator operates on. Chora Validator is operated by a team of senior software engineers with more than two years of experience building, maintaining, and testing software within Cosmos ecosystem.'}
+          </p>
+          <h3>{'Regen'}</h3>
+          <p>
+            {
+              'Chora Validator is running a validator node and a full node with public API endpoints. Chora Validator is also running a testnet validator node on Regen Redwood (the stable test network for Regen). Chora Validator is committed to offsetting energy costs with carbon credits issued on Regen.'
             }
           </p>
         </div>

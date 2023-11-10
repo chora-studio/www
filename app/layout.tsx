d5c629db4aa6153362a31857b25d841b9588c0aa
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2023-11-09 21:38:05 -0800

    use chora web

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 'use client'
 
+import { Footer, Header } from 'chora/components'
 import { ThemeContextProvider } from 'chora/contexts'
-
-import Footer from '@components/Footer'
-import Header from '@components/Header'
 
 import './globals.css'
 
@@ -12,9 +10,102 @@ const Layout = ({ children }: any) => (
     <body>
       <main>
         <ThemeContextProvider>
-          <Header />
+          <Header
+            title={{
+              link: '/',
+              titleX: 'studio',
+            }}
+            itemsLeft={[
+              {
+                link: '/products',
+                title: 'products',
+              },
+              {
+                link: '/solutions',
+                title: 'solutions',
+              },
+              {
+                link: '/validator',
+                title: 'validator',
+              },
+            ]}
+            itemsRight={[
+              {
+                link: 'https://chora.blog',
+                title: 'blog ↗',
+              },
+            ]}
+          />
           {children}
-          <Footer />
+          <Footer
+            lists={[
+              {
+                title: 'chora studio',
+                items: [
+                  {
+                    link: 'https://chora.blog',
+                    target: '_blank',
+                    title: 'blog ↗',
+                  },
+                  {
+                    link: '/products',
+                    target: '',
+                    title: 'products',
+                  },
+                  {
+                    link: '/solutions',
+                    target: '',
+                    title: 'solutions',
+                  },
+                  {
+                    link: '/validator',
+                    target: '',
+                    title: 'validator',
+                  },
+                ],
+              },
+              {
+                title: 'chora protocol',
+                items: [
+                  {
+                    link: 'https://chora.io',
+                    target: '_blank',
+                    title: 'chora applications ↗',
+                  },
+                  {
+                    link: 'https://docs.chora.io',
+                    target: '_blank',
+                    title: 'chora documentation ↗',
+                  },
+                  {
+                    link: 'https://github.com/chora-io',
+                    target: '_blank',
+                    title: 'chora repositories ↗',
+                  },
+                ],
+              },
+              {
+                title: 'chora validator',
+                items: [
+                  {
+                    link: 'https://www.mintscan.io/regen/validators/regenvaloper1t8p3zdu3h8qzggfmvvvmtdnaj4trcsfh79xp4e',
+                    target: '_blank',
+                    title: 'regen ↗',
+                  },
+                  {
+                    link: 'https://chora.io/scan/regen-redwood-1',
+                    target: '_blank',
+                    title: 'regen-redwood ↗',
+                  },
+                  {
+                    link: 'https://chora.io/scan/chora-testnet-1',
+                    target: '_blank',
+                    title: 'chora-testnet ↗',
+                  },
+                ],
+              },
+            ]}
+          />
         </ThemeContextProvider>
       </main>
     </body>
